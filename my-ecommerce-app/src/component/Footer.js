a2670be1648f8df49613a30054eaede04f47e16d
Ayodeji <danielfatokun@gmail.com>
2024-03-25 17:51:15 -0600

    Delete Homepage.js and Footer.js

---
+++ my-ecommerce-app/src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Copyright, Jacelynn Doan, Ayodeji Fatokun</p>
+    </footer>
+  );
+}
+
+export default Footer;
