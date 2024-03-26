7512357dfa1465c2d81d269381e5a8b40198178a
Jacelynn Doan <jacelynn.doan123@gmail.com>
2024-03-25 21:58:42 -0600

    updates

--- my-ecommerce-app/src/component/ProductList.js
+++ my-ecommerce-app/src/component/ProductList.js
@@ -2,10 +2,10 @@ import React from "react";
 import ProductItem from "./ProductItem";
 import products from "../data/Products";
 
-function ProductList(props) {
+function ProductList(props) { // props is passed from the parent component
   return (
-    <div className="product-list">
-      <ProductItem products={products} handleCartAdd={props.handleCartAdd} />
+    <div className="product-list"> 
+      <ProductItem products={products} handleCartAdd={props.handleCartAdd} /> {/* pass the products and handleCartAdd function to the ProductItem component */}
     </div>
   );
 }
