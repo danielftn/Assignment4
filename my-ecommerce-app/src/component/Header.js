1e13434cfea0c33b44d9752fef13c96a6021396b
Ayodeji <danielfatokun@gmail.com>
2024-03-25 21:47:09 -0600

    Update Header.js

--- my-ecommerce-app/src/component/Header.js
+++ my-ecommerce-app/src/component/Header.js
@@ -4,19 +4,18 @@ import {Link} from 'react-router-dom';
 function Header() {
     return (
         <div>
-            <header class="header">
+            <header class="header" style={{display: 'flex', justifyContent: 'space-between'}}>
                 <div class="logo">
                     <img src={process.env.PUBLIC_URL + "images/logo.png"} height={"200px"} alt="Company Logo"></img>
-                    {}
                 </div>
                 <div class="company-name">
-                    Company Name
+                    <p>Company Name</p>
                 </div>
             </header>
-            <nav>
-                <Link to ="./" class="nav-link">Home        </Link>
-                <Link to ="./ProductPage" class="nav-link">Products     </Link>
-                <Link to ="./" class="nav-link">Login       </Link>
+            <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Link to ="./" class="nav-link">Home</Link>
+                <Link to ="./ProductPage" class="nav-link">Products</Link>
+                <Link to ="./" class="nav-link">Login</Link>
             </nav>
         </div>
     );
