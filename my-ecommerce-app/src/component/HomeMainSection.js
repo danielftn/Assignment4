1a91eff8274c5033902698cd243e87970cc50ef8
Jacelynn Doan <jacelynn.doan123@gmail.com>
2024-03-25 20:46:35 -0600

    modify HomeMainSection

--- my-ecommerce-app/src/component/HomeMainSection.js
+++ my-ecommerce-app/src/component/HomeMainSection.js
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from "react";
 import reviews from "../data/Reviews.js";
 
 function HomeMainSection() {
-  const [Reviews, setReviews] = useState([]);
+  const [Reviews, setReviews] = useState([]); // state to store selected reviews
 
-  useEffect(() => {
-    const randomReviews = [];
-    for (let i = 0; i < 2; i++) {
-      const randomIndex = Math.floor(Math.random() * reviews.length); 
-      randomReviews.push(reviews[randomIndex]);
+  useEffect(() => { // select 2 random reviews from the reviews array
+    const selectedReviews = [];
+    for (let j = 0; j < 2; j++) {
+      const randomI = Math.floor(Math.random() * reviews.length);  // generate random index
+      selectedReviews.push(reviews[randomI]); // add the review to the selectedReviews array
     }
-    setReviews(randomReviews); 
+    setReviews(selectedReviews); // update the state with the selected reviews
   }, []);
 
   return (
     <main>
       {}
-      <section class="about-us">
-        <h2>About Us</h2>
+      <section class="about-us"> 
+        <h2>About Us</h2> 
         <p>Welcome to our online store! We are passionate about providing high-quality products and exceptional customer service.
         Learn more about our story and commitment to your satisfaction.
         </p>
@@ -28,15 +28,15 @@ function HomeMainSection() {
         <button>
           <a href="products.html">Shop Now</a>
         </button>
-      </section>
+      </section>  
 
       {}
-      <section class="customer-reviews">
-        {Reviews.map((review, index) => (
-          <div key={index} class="review">
-            <h2>{review.customerName}</h2>
+      <section class="customer-reviews">  
+        {Reviews.map((review, i) => ( // display the selected reviews
+          <div key={i} class="review">
+            <h2>{review.customerName}</h2>  
             <p>{review.reviewContent}</p>
-            <p>Rating: {"⭐".repeat(review.stars)}</p>{" "}
+            <p>Rating: {"⭐".repeat(review.stars)}</p>{" "} 
             {}
           </div>
         ))}
