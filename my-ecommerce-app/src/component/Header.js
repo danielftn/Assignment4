2d4cfd6dbf65cebc7c8c981cdc1a1b87e4ab7de5
Ayodeji <danielfatokun@gmail.com>
2024-03-25 22:18:49 -0600

    Update Header.js

--- my-ecommerce-app/src/component/Header.js
+++ my-ecommerce-app/src/component/Header.js
@@ -4,17 +4,18 @@ import {Link} from 'react-router-dom';
 function Header() {
     return (
         <div>
-            <header class="header" style={{display: 'flex', justifyContent: 'space-between'}}>  {/* inline style */}
+            <header style={{display: 'flex', justifyContent: 'space-between'}}>  {/* inline style */}
                 <div class="logo">
                     <img src={process.env.PUBLIC_URL + "images/logo.png"} height={"200px"} alt="Company Logo"></img>    {/* use process.env.PUBLIC_URL to access the public folder */}
                 </div>
-                <div class="company-name">  
+                <div>  
                     <p>Company Name</p>
                 </div>
             </header>
                 <nav style={{ display: 'flex', justifyContent: 'space-between' }}>  {/* inline style */}
-                <Link to ="./ProductPage" class="nav-link">Products</Link>  {/* use Link to navigate to the ProductPage component */}
-                <Link to ="./" class="nav-link">Login</Link>    {/* use Link to navigate to the Homepage component */}
+                <Link to ="/">Homepage</Link>
+                <Link to ="/ProductPage">Products</Link>  {/* use Link to navigate to the ProductPage component */}
+                <Link to ="/">Login</Link>    {/* use Link to navigate to the Homepage component */}
             </nav>
         </div>
     );
