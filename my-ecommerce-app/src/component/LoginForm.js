2b6e6f0b939db443ab854b6043b08b834dcb83f3
Ayodeji <danielfatokun@gmail.com>
2024-04-01 23:09:44 -0600

    Updated Login.js and Signup.js

--- my-ecommerce-app/src/component/LoginForm.js
+++ my-ecommerce-app/src/component/LoginForm.js
@@ -6,36 +6,45 @@ function LoginForm() {
 
     const[username, setUsername] = useState('')
     const[password, setPassword] = useState('')
-    const [login, setLogin] = useState(true);
+    const[login, setLogin] = useState(true);
 
     const handleLogin = () => {
         if('' === username){
             alert('Please enter your username');
-            return;
+            return
         }
 
         if('' === password) {
             alert('Please enter your password')
+            return
         }
     }
 
     const handleSwitch = () => {
         setLogin(!login);
+
+        if(login === true){
+            <LoginForm/>
+        }
+
+        if(login === false){
+            <SignupForm/>
+        }
     }
 
     return(
         <form className='loginForm'>
             <h1>Login</h1>
             <div className='Username'>
-                <p>Login: <input placeholder='Enter your username' 
-                className='inputBox' 
+                <p>Username: <input placeholder='Enter your username' 
+                className='Username' 
                 type='text'
                 onChange={e => setUsername(e.target.value)} /></p>
             </div>
             
             <div className='Password'>
                 <p>Password: <input placeholder='Enter your password' 
-                className='inputBox' 
+                className='Password' 
                 type='password' 
                 onChange={e => setPassword(e.target.value)} /></p>
             </div>
@@ -45,7 +54,9 @@ function LoginForm() {
             </div>
 
             <div className='switchtoSignup'>
-                <input type='button' value={'Switch to Signup'} onClick={handleSwitch}/>
+                <input type='button' 
+                value={'Switch to Signup'} 
+                onClick={handleSwitch}/>
             </div>
         </form>
     );
--- my-ecommerce-app/src/component/SignupForm.js
+++ my-ecommerce-app/src/component/SignupForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import LoginForm from './LoginForm';
 
 function SignupForm(){
     const[username, setUsername] = useState('')
@@ -26,24 +27,26 @@ function SignupForm(){
     }
 
     return(
-    <div className='signupForm'>
+    <form className='signupForm'>
         <div className='Username'>
-            <p>Login: <input placeholder='Enter your username' 
+            <p>Username: 
+            <input placeholder='Enter your username' 
             className='Username' 
             type='text' 
             onChange={e => setUsername(e.target.value)} /></p>
         </div>
         
         <div className='Password'>
-            <p>Password: <input 
-            placeholder='Enter your password' 
+            <p>Password: 
+            <input placeholder='Enter your password' 
             className='Password' 
             type='password' 
             onChange={e => setPassword(e.target.value)}/></p>
         </div>
 
         <div className='confirmPassword'>
-            <p>Confirm Password: <input placeholder='Confirm your password' 
+            <p>Confirm Password: 
+            <input placeholder='Confirm your password' 
             className='confirmPassword' 
             type='password' 
             onChange={e => setConfirmPassword(e.target.value)}/></p>
@@ -63,7 +66,7 @@ function SignupForm(){
         <div className='switchtoLogin'>
             <input type='button' value={'Switch to Login'}/>
         </div>
-    </div>
+    </form>
     );
 }
 export default SignupForm;
